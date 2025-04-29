a0f75f8e1292ae0f57b70bcb43848bf7039d04e5
nnlinh97 <35454352+nnlinh97@users.noreply.github.com>
2025-04-29 16:25:49 +0700

    update screen size

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,9 +8,10 @@ module.exports = {
       xs: '576',
       sm: '640px',
       md: '768px',
-      lg: '1024px',
-      xl: '1280px',
-      '2xl': '1536px'
+      lg: '1280px',
+      xl: '1440px',
+      '2xl': '1600px',
+      '3xl': '1920px'
     },
     extend: {
       fontFamily: {
